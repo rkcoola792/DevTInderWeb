e2e03c32b8d1b7542cdd105f99d0a72d33108c19
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-11-18 12:30:28 +0530

    routing created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
-import './App.css'
+import "./App.css";
+import Body from "./Body";
+import Navbar from "./components/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-   <div className='text-3xl'>hi</div>
-  )
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<h1>login</h1>} />
+            <Route path="/feed" element={<h1>feed</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <div className="text-3xl ">{/* <Navbar /> */}</div>
+    </>
+  );
 }
 
-export default App
+export default App;
