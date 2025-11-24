45c4e9d1984ed861a3bf6a1c73b0bb7ca340e04a
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-11-24 16:56:09 +0530

    feed api integrated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,14 +2,15 @@ import React, { useEffect } from "react";
 import Feedcard from "./Feedcard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../store/feedSlice";
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
   const getFeed = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/feed", {
+      const res = await axios.get(BASE_URL + "/feed?page=1&limit=10", {
         withCredentials: true,
       });
       dispatch(addFeed(res.data));
@@ -20,7 +21,7 @@ const Feed = () => {
   useEffect(() => {
     getFeed();
   }, []);
-  return <Feedcard name="John Doe" age="20" />;
+  if (feed) return feed.feed.map((feed) => <Feedcard feed={feed} />);
 };
 
 export default Feed;
--- src/components/Feedcard.jsx
+++ src/components/Feedcard.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Feedcard = ({name,age}) => {
+const Feedcard = ({ feed }) => {
+  const { name, age, profileImage } = feed;
+  console.log("profileImage", profileImage);
   return (
-  <div className="card bg-base-300 w-96 shadow-sm my-20 mx-auto h-1/2">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-     {name}
-    </h2>
-    <p>{age}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Ignore</button>
-      <button className="btn btn-secondary">Interested</button>
+    <div className="card bg-base-300 w-96 shadow-sm my-20 mx-auto h-1/2">
+      <figure>
+        <img
+          src={
+            profileImage ||
+            "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          }
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{age}</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default Feedcard
+export default Feedcard;
